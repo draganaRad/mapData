bd8970f168889d709565280a4011da9b4316cf38
Dragana Radulovic <draganaradulovic@Draganas-MBP.vc.shawcable.net>
2021-05-08 15:12:26 -0700

    added minZoom

--- src/stressmap.js
+++ src/stressmap.js
@@ -33,7 +33,8 @@ L.tileLayer(
   'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
   subdomains: 'abcd',
-  maxZoom: 19
+  maxZoom: 19,
+  minZoom: 9
 }
 ).addTo(map);
 // Add BikeOttawa attribution
